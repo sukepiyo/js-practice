1d3a7920fa18551280969629da920acad8bf07a0
sukepiyo <keiuskekudo0828@gmail.com>
2024-03-16 22:58:38 +0900

    スクロールアニメーションの実装

--- js/script.js
+++ js/script.js
@@ -41,3 +41,33 @@ accordionTitles.forEach((accordionTitle) => {
         content.classList.toggle('active');
     });
 });
+
+/* スクロールアニメーション
+================================================== */
+// 監視対象が範囲内に現れたら実行する動作
+const animateFade = (entries, obs) => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            entry.target.animate(
+                {
+                    opacity: [0, 1],
+                },
+                {
+                    duration: 2000,
+                    delay: 1000 * index,
+                    fill: 'forwards'
+                }
+            );
+            // 一度ふわっと表示されたら監視をやめる
+            obs.unobserve(entry.target);
+        }
+    });
+};
+// 監視設定
+const fadeObserver = new IntersectionObserver(animateFade);
+
+// .fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+    fadeObserver.observe(fadeElement);
+});
